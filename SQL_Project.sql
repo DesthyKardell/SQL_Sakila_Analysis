0d949d86c4bd3724ef90eb601d0a5dfe42474e93
DMatouba <46376708+DesthyKardell@users.noreply.github.com>
2019-07-22 11:33:43 -0400

    Update SQL_Project.sql

--- SQL_Project.sql
+++ SQL_Project.sql
@@ -1,66 +1,59 @@
 USE sakila;
 
-# 1a. Display the first and last names of all actors from the table actor
+# 1a. Displaying the first and last names of all actors from the table actor
 SELECT first_name, last_name
 FROM actor;
 
-# 1b. Display the first and last name of each actor in a single column in upper 
-# case letters. Name the column Actor Name
+# 1b. Displaying the first and last name of each actor in a single column named Actor_Name
 USE sakila;
 SELECT concat(first_name, " ", last_name) AS Actor_Name
 FROM actor;
 
-# 2a. You need to find the ID number, first name, 
-# and last name of an actor, of whom you know only the first name, "Joe." 
-# What is one query would you use to obtain this information?
+# 2a. Displaying the ID number, first name, 
+# and last name of an actor whose first name is "Joe." 
 USE Sakila;
 SELECT actor_id, first_name, last_name
 FROM actor
 WHERE first_name = 'Joe';
 
-# 2b. Find all actors whose last name contain the letters GEN
+# 2b. Finding all actors whose last name contain the letters GEN
 USE sakila;
 SELECT * FROM actor
 WHERE last_name LIKE '%GEN%';
 
-# 2c. Find all actors whose last names contain the letters LI. 
-# This time, order the rows by last name and first name, in that order
+# 2c. Finding all actors whose last names contain the letters LI. 
+# Ordering results by last name and first name
 USE sakila;
 SELECT * FROM actor
 WHERE last_name LIKE '%LI%'
 ORDER BY last_name, first_name;
 
-# 2d. Using IN, display the country_id and country columns 
+# 2d. Displaying the country_id  
 # of the following countries: Afghanistan, Bangladesh, and China:
 USE sakila;
 SELECT country_id, country FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
-# 3a. You want to keep a description of each actor. 
-# You don't think you will be performing queries on a description, 
-# so create a column in the table actor named description 
-# and use the data type BLOB (Make sure to research the type BLOB, 
-# as the difference between it and VARCHAR are significant).
+# 3a. Altering the Actor table to add a Description Field. 
 USE sakila;
 ALTER TABLE actor
 ADD Description BLOB;
 SELECT * FROM actor;
 
-# 3b. Very quickly you realize that entering descriptions 
-# for each actor is too much effort. Delete the description column.
+# 3b. Deleting the Description column previously created
 USE sakila;
 ALTER TABLE actor
 DROP COLUMN Description;
 SELECT * FROM actor;
 
-# 4a. List the last names of actors, 
+# 4a. Listing the last names of actors, 
 # as well as how many actors have that last name.
 USE sakila;
 SELECT last_name, COUNT(last_name) as "Count of Last Name"
 FROM actor
 GROUP BY last_name;
 
-# 4b. List last names of actors and the number of actors 
+# 4b. Listing last names of actors and the number of actors 
 # who have that last name, but only for names that are shared by at least two actors
 USE sakila;
 SELECT last_name, COUNT(last_name) as "Count of Last Name"
@@ -68,26 +61,22 @@ FROM actor
 GROUP BY last_name
 HAVING COUNT(last_name) > 1;
 
-# 4c. The actor HARPO WILLIAMS was accidentally entered 
-# in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
+# 4c. Writing a query to change actor name.
+# It was accidently entered as GROUCHO WILLIAMS but should be HARPO WILLIAMS
 USE sakila;
 UPDATE actor
 SET first_name='HARPO'
 WHERE first_name = 'GROUCHO' AND last_name = 'WILLIAMS';
 SELECT * FROM actor;
 
-# 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. 
-# It turns out that GROUCHO was the correct name after all! 
-# In a single query, if the first name of the actor 
-# is currently HARPO, change it to GROUCHO.
+# 4d. Reversing 4c by changing first name to GROUCHO from HARPO 
 USE sakila;
 UPDATE actor
 SET first_name = 'GROUCHO'
 WHERE first_name = 'HARPO';
 SELECT * FROM actor;
 
-# 5a. You cannot locate the schema of the address table. 
-# Which query would you use to re-create it?
+# 5a. Rebuilding the address table, which I am unable to recreate. 
 USE sakila;
 Describe address;
 # By using SHOW create table, I will be able to see the syntax of the table
@@ -100,26 +89,25 @@ CREATE TABLE address (
   district VARCHAR(20) NOT NULL,
   city_id SMALLINT(5) unsigned NOT NULL);
   
-# 6a. Use JOIN to display the first and last names, 
-# as well as the address, of each staff member. 
-# Use the tables staff and address:
+# 6a. Displaying the first and last names, as of well as addresses
+# of each staff member, using JOIN on tables staff and address:
 USE sakila;
 SELECT first_name, last_name, address
 FROM staff as s
 INNER JOIN address as a
 ON s.address_id = a.address_id;
 
-# 6b. Use JOIN to display the total amount rung up 
-# by each staff member in August of 2005. Use tables staff and payment
+# 6b. Displaying the total amount rung up by each staff members using JOIN
+# on tables staff and payment
 USE sakila;
-SELECT first_name, last_name, SUM(amount)
+SELECT s.first_name, s.last_name, SUM(p.amount)
 FROM staff as s
 INNER JOIN payment as p
 ON s.staff_id = p.staff_id
 GROUP BY p.staff_id;
 
-# 6c. List each film and the number of actors 
-# who are listed for that film. Use tables film_actor and film. Use inner join
+# 6c. Listing each film and the number of actors 
+# who are listed for that film. Using Inner Join on tables film_actor and film
 USE sakila;
 SELECT f.title, COUNT(fa.actor_id)
 FROM film as f
@@ -127,7 +115,7 @@ INNER JOIN film_actor as fa
 ON f.film_id = fa.film_id
 GROUP BY f.title;
 
-# 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+# 6d. Displaying the number of copies of the film "Hunchback Impossible" in the inventory system?
 USE sakila;
 SELECT f.title, COUNT(i.inventory_id)
 FROM film as f
@@ -136,8 +124,8 @@ ON f.film_id = i.film_id
 WHERE f.title = 'Hunchback Impossible';
 
 #6e. Using the tables payment and customer and the JOIN command, 
-# list the total paid by each customer. 
-# List the customers alphabetically by last name:
+# listing the total paid by each customer. 
+# Listing the customers alphabetically by last name:
 USE sakila;
 SELECT c.first_name, c.last_name, SUM(p.amount) as 'Total Amount Paid'
 FROM customer as c
@@ -149,7 +137,7 @@ ORDER BY c.last_name;
 # 7a. The music of Queen and Kris Kristofferson 
 # have seen an unlikely resurgence. As an unintended consequence, 
 # films starting with the letters K and Q have also soared in popularity. 
-# Use subqueries to display the titles of movies starting with the letters K and Q 
+# Using subqueries to display the titles of movies starting with the letters K and Q 
 # whose language is English.
 USE sakila;
 SELECT title FROM film
@@ -159,8 +147,8 @@ WHERE language_id in
     WHERE name = 'English')
 AND (title like 'K%') OR (title LIKE 'Q%');
 
-# 7b. Use subqueries to display all actors
-#  who appear in the film Alone Trip
+# 7b. Using subqueries to display all actors
+#  who appear in the film "Alone Trip"
 USE sakila;
 SELECT first_name, last_name
 FROM actor
@@ -170,9 +158,8 @@ WHERE actor_id in
 		(SELECT film_id FROM film
         WHERE title = "Alone Trip"));
 	
-# 7c. You want to run an email marketing campaign in Canada, 
-# for which you will need the names and email addresses 
-# of all Canadian customers. Use joins to retrieve this information
+# 7c. Displaying names and email addresses of all canadian customers
+# Using joins to retrieve this information
 USE sakila;
 SELECT c.first_name, c.last_name, c.email
 FROM customer as c
@@ -180,9 +167,8 @@ INNER JOIN address as a ON c.address_id = a.address_id
 INNER JOIN city as ci ON a.city_id = ci.city_id
 INNER JOIN country as co ON ci.country_id = co.country_id WHERE country = 'Canada';
 
-# 7d. Sales have been lagging among young families, 
-# and you wish to target all family movies for a promotion. 
-# Identify all movies categorized as family films
+# 7d. Identifying all movies categorized as family films to conduct a promotion.
+# Sales have been lagging among young families, 
 USE sakila;
 SELECT title 
 FROM film
@@ -192,7 +178,7 @@ WHERE film_id in
 		(SELECT category_id FROM category 
         WHERE name = 'family'));
         
-# 7e. Display the most frequently rented movies in descending order
+# 7e. Displaying the most frequently rented movies in descending order
 USE sakila;
 SELECT i.film_id, f.title, COUNT(r.inventory_id)
 FROM inventory as i
@@ -203,7 +189,7 @@ ON i.film_id = f.film_id
 GROUP BY r.inventory_id
 ORDER BY COUNT(r.inventory_id) DESC;
 
-# 7f. Write a query to display how much business,
+# 7f. Writing a query to display how much business,
 # in dollars, each store brought in
 USE sakila;
 SELECT store.store_id, SUM(payment.amount)
@@ -215,7 +201,7 @@ ON payment.staff_id = staff.staff_id
 GROUP BY store.store_id
 ORDER BY SUM(payment.amount);
 
-# 7g. Write a query to display for each store 
+# 7g. Writing a query to display for each store 
 # its store ID, city, and country
 USE sakila;
 SELECT store.store_id, city.city, country.country
@@ -227,10 +213,9 @@ ON address.city_id = city.city_id
 INNER JOIN country
 ON city.country_id = country.country_id;
 
-# 7h. List the top five genres in gross revenue 
-# in descending order. (Hint: you may need to use 
-# the following tables: category, film_category, 
-# inventory, payment, and rental.)
+# 7h. Listing the top five genres in gross revenue 
+# in descending order. (Use the following tables:
+# category, film_category, inventory, payment, and rental.)
 USE sakila;
 SELECT category.name, SUM(payment.amount)
 FROM category
@@ -246,10 +231,10 @@ GROUP BY category.name
 ORDER BY SUM(payment.amount) DESC
 LIMIT 5;
 
-# 8a. In your new role as an executive, you would like
+# 8a. Creating a View using 7h.
+# Scenario: In my new role as an executive, I would like
 # to have an easy way of viewing the Top five genres by gross revenue. 
-# Use the solution from the problem above to create a view. 
-# If you haven't solved 7h, you can substitute another query to create a view
+
 USE sakila;
 
 CREATE VIEW top_five_genres_revenues AS
@@ -267,11 +252,10 @@ GROUP BY category.name
 ORDER BY SUM(payment.amount) DESC
 LIMIT 5;
 
-# 8b. How would you display the view that you created in 8a?
+# 8b. Displaying the view created in 8a
 SELECT * FROM top_five_genres_revenues;
 
-# 8c. You find that you no longer need the view top_five_genres. 
-# Write a query to delete it.
+# 8c. Query to delete view created above: top_five_genres. 
 DROP VIEW top_five_genres_revenues;
 
 
